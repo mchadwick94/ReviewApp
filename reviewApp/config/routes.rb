b713bd3e397dcf45c6b84d928af53aaee2987fd2
b6035379 <b6035379@my.shu.ac.uk>
2019-04-18 02:03:28 +0100

    Created Sessions and ability to log in/out.

--- reviewApp/config/routes.rb
+++ reviewApp/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'sessions/index'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   get 'static_pages/home'
   root 'static_pages#home'
   get 'static_pages/about'
@@ -12,5 +16,6 @@ end
   resources :reviews
   resources :profiles
   resources :users
+  resources :sessions
 
 end
